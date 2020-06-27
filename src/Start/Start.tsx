8d60e8173d08c44406fdebf0df286b6b442a4f3f
irenee <irenee@qchi.net>
2020-06-27 17:03:10 +0100

    style: improved start screen

--- src/Start/Start.tsx
+++ src/Start/Start.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
+import Svg, { Defs, RadialGradient, Stop, Ellipse } from "react-native-svg";
 
 import Button from "../components/Button";
 import BackgroundScreen from "../components/BackgroundScreen";
@@ -7,6 +8,7 @@ import BackgroundScreen from "../components/BackgroundScreen";
 import { getCountryQuizGame } from "../generateQuiz/countryQuiz";
 import { filterCountries } from "../generateQuiz/filterCountries";
 import allCountries from "../../data/countries.json";
+import { globalStyles, colors } from "../styles/globals";
 
 type Props = {
   navigation: any;
@@ -23,6 +25,18 @@ const Start: React.FC<Props> = (props) => {
 
   return (
     <BackgroundScreen testID="startScreen">
+      <View style={styles.imageContainer}>
+        <Image source={require("../../assets/img/trivia_icon.png")} style={{ width: 300, height: 300 }} />
+        <Svg height="80" width="400">
+          <Defs>
+            <RadialGradient id="grad" cx="200" cy="40" rx="100" ry="10" fx="200" fy="40" gradientUnits="userSpaceOnUse">
+              <Stop offset="0" stopColor="#000" stopOpacity="0.2" />
+              <Stop offset="1" stopColor={colors.background} stopOpacity="0.2" />
+            </RadialGradient>
+          </Defs>
+          <Ellipse cx="200" cy="40" rx="100" ry="10" fill="url(#grad)" />
+        </Svg>
+      </View>
       <View style={styles.btnContainer}>
         <Button testID="startBtn" onPress={onStart} label="Start" />
       </View>
@@ -34,6 +48,16 @@ const styles = StyleSheet.create({
   btnContainer: {
     paddingHorizontal: 20,
     width: "100%",
+    flex: 1,
+  },
+  imageContainer: {
+    ...globalStyles.centredContainer,
+    flex: 2,
+  },
+  iconShadow: {
+    width: "25%",
+    height: 40,
+    backgroundColor: "black",
   },
 });
 
